a6ad1a6164c59ec7ab953a59f147ab271ef6fb28
Valery Bugakov <skymk1@gmail.com>
2024-03-20 14:21:18 +0800

    Add dart grammar

--- build.ts
+++ build.ts
@@ -15,32 +15,32 @@ const outDir = path.join(__dirname, "out");
 let hasErrors = false;
 
 async function buildParserWASM(
-  name: string,
-  { subPath, generate }: { subPath?: string; generate?: boolean } = {}
+	name: string,
+	{ subPath, generate }: { subPath?: string; generate?: boolean } = {},
 ) {
-  const label = subPath ? path.join(name, subPath) : name;
-  try {
-    console.log(`⏳ Building ${label}`);
-    let packagePath;
-    try {
-      packagePath = findRoot(require.resolve(name));
-    } catch (_) {
-      packagePath = path.join(__dirname, "node_modules", name);
-    }
-    const cwd = subPath ? path.join(packagePath, subPath) : packagePath;
-    if (generate) {
-      await exec(`pnpm tree-sitter generate`, { cwd });
-    }
-    await exec(`pnpm tree-sitter build-wasm ${cwd}`);
-    console.log(`✅ Finished building ${label}`);
-  } catch (e) {
-    console.error(`🔥 Failed to build ${label}:\n`, e);
-    hasErrors = true;
-  }
+	const label = subPath ? path.join(name, subPath) : name;
+	try {
+		console.log(`⏳ Building ${label}`);
+		let packagePath: string;
+		try {
+			packagePath = findRoot(require.resolve(name));
+		} catch (_) {
+			packagePath = path.join(__dirname, "node_modules", name);
+		}
+		const cwd = subPath ? path.join(packagePath, subPath) : packagePath;
+		if (generate) {
+			await exec("pnpm tree-sitter generate", { cwd });
+		}
+		await exec(`pnpm tree-sitter build-wasm ${cwd}`);
+		console.log(`✅ Finished building ${label}`);
+	} catch (e) {
+		console.error(`🔥 Failed to build ${label}:\n`, e);
+		hasErrors = true;
+	}
 }
 
 if (fs.existsSync(outDir)) {
-  fs.rmSync(outDir, { recursive: true, force: true });
+	fs.rmSync(outDir, { recursive: true, force: true });
 }
 
 fs.mkdirSync(outDir);
@@ -48,28 +48,28 @@ fs.mkdirSync(outDir);
 process.chdir(outDir);
 
 const grammars = Object.keys(packageInfo.devDependencies).filter(
-  (n) => n.startsWith("tree-sitter-") && n !== "tree-sitter-cli"
+	(n) => n.startsWith("tree-sitter-") && n !== "tree-sitter-cli",
 );
 
 PromisePool.withConcurrency(os.cpus().length)
-  .for(grammars)
-  .process(async (name) => {
-    if (name == "tree-sitter-rescript") {
-      await buildParserWASM(name, { generate: true });
-    } else if (name == "tree-sitter-ocaml") {
-      await buildParserWASM(name, { subPath: "ocaml" });
-    } else if (name == "tree-sitter-php") {
-      await buildParserWASM(name, { subPath: "php" });
-    } else if (name == "tree-sitter-typescript") {
-      await buildParserWASM(name, { subPath: "typescript" });
-      await buildParserWASM(name, { subPath: "tsx" });
-    } else {
-      await buildParserWASM(name);
-    }
-  })
-  .then(async () => {
-    if (hasErrors) {
-      process.exit(1);
-    }
-    await exec(`mv *.wasm ${outDir}`, { cwd: __dirname });
-  });
+	.for(grammars)
+	.process(async (name) => {
+		if (name === "tree-sitter-rescript") {
+			await buildParserWASM(name, { generate: true });
+		} else if (name === "tree-sitter-ocaml") {
+			await buildParserWASM(name, { subPath: "ocaml" });
+		} else if (name === "tree-sitter-php") {
+			await buildParserWASM(name, { subPath: "php" });
+		} else if (name === "tree-sitter-typescript") {
+			await buildParserWASM(name, { subPath: "typescript" });
+			await buildParserWASM(name, { subPath: "tsx" });
+		} else {
+			await buildParserWASM(name);
+		}
+	})
+	.then(async () => {
+		if (hasErrors) {
+			process.exit(1);
+		}
+		await exec(`mv *.wasm ${outDir}`, { cwd: __dirname });
+	});
